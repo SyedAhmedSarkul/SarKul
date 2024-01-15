2b2ae2d610a9f0422e33d1f7e63b36ef0175094e
Tosmim <tosmimforidmehtab@gmail.com>
2024-01-15 20:51:29 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use("/api/v1/user", userRoutes);
 
