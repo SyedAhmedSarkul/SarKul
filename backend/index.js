bc52180121c32478039edab5f406f2ee475f0e23
Tosmim <tosmimforidmehtab@gmail.com>
2024-01-15 10:44:26 +0530

    DB setup and connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
