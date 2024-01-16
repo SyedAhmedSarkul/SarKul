871d932285aaa62753660be51b3f5c5995e115ba
FaizanHashmi3 <mdfaizanhashmi3@gmail.com>
2024-01-16 13:07:40 +0530

    login page done

--- frontend/sarkul-front-end/src/App.js
+++ frontend/sarkul-front-end/src/App.js
@@ -1,12 +1,17 @@
 
+import { useState } from 'react';
 import './App.css';
 import Login from './components/Login';
+import Home from './components/Pages/Home';
+
 
 function App() {
+  const [isUser,setIsUser] = useState(false);
   return (
     <div className="App">
 
-    <Login/>
+    {!isUser? ( <Login setIsUser={setIsUser}/>):(<Home/>)}
+   
     </div>
   );
 }
