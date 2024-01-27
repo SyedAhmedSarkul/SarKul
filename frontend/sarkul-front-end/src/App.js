23c3b82101aa7f40ef8f5381cb3a58f390c0ffc9
FaizanHashmi3 <mdfaizanhashmi3@gmail.com>
2024-01-27 22:20:00 +0530

    login add

--- frontend/sarkul-front-end/src/App.js
+++ frontend/sarkul-front-end/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
      
-    {isUser? ( <Login setIsUser={setIsUser}/>):(
+    {!isUser? ( <Login setIsUser={setIsUser}/>):(
     
     <div>
       <Home/>
