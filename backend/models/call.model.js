77d2d7d2ee00ef2d4c12b997a095122ed2ff8dc3
Tosmim <tosmimforidmehtab@gmail.com>
2024-01-31 13:16:10 +0530

    Client Requirements changed for Engineer. So here I am

--- backend/models/call.model.js
+++ backend/models/call.model.js
@@ -1,82 +1,93 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import validator from "validator";
 
-const callSchema = new Schema({
-    callId: {
-        type: String,
-        unique: [true, "Call ID must be unique"],
-        index: true
+const callSchema = new Schema(
+    {
+        callId: {
+            type: String,
+            unique: [true, "Call ID must be unique"],
+            index: true,
+        },
+        serialNumber: {
+            type: String,
+        },
+        customerName: {
+            type: String,
+            required: [true, "Customer name is required"],
+        },
+        customerCode: {
+            type: String,
+            required: [true, "Customer code is required"],
+        },
+        contactNumber: {
+            type: String,
+            required: [true, "Contact number is required"],
+            validate: [validator.isMobilePhone, "Invalid mobile number"],
+        },
+        customerEmail: {
+            type: String,
+            required: [true, "Customer email is required"],
+            validate: [validator.isEmail, "Invalid email"],
+        },
+        customerAddress: {
+            type: String,
+            required: [true, "Customer address is required"],
+        },
+        problemDescription: {
+            type: String,
+            required: [true, "Problem description is required"],
+        },
+        status: {
+            type: String,
+            enum: ["pending", "completed"],
+            default: "pending",
+        },
+        category: {
+            type: String,
+            required: [true, "Category is required"],
+            enum: {
+                values: [
+                    "desktop",
+                    "laptop",
+                    "printer",
+                    "plotter",
+                    "scanner",
+                    "server",
+                ],
+                message: "Invalid category",
+            },
+        },
+        itemName: {
+            type: String,
+            required: [true, "Item name is required"],
+        },
+        itemModelNumber: {
+            type: String,
+        },
+        engineerName: {
+            type: String,
+        },
+        engineersAssigned: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Engineer",
+            },
+        ],
+        closedAt: {
+            type: Date,
+        },
+        customerRemark: {
+            type: String,
+        },
+        engineerRemark: {
+            type: String,
+        },
+        itemStatus: {
+            type: String,
+            enum: ["required", "pending"],
+        },
     },
-    serialNumber: {
-        type: String,
-    },
-    customerName: {
-        type: String,
-        required: [true, "Customer name is required"],
-    },
-    customerCode: {
-        type: String,
-        required: [true, "Customer code is required"],
-    },
-    contactNumber: {
-        type: String,
-        required: [true, "Contact number is required"],
-        minLength: [10, "Contact number must be minimum 10 digits"],
-        maxLength: [14, "Contact number must be maximum 14 digits"],
-    },
-    customerEmail: {
-        type: String,
-        required: [true, "Customer email is required"],
-        validate: [validator.isEmail, "Invalid email"],
-    },
-    customerAddress: {
-        type: String,
-        required: [true, "Customer address is required"],
-    },
-    problemDescription: {
-        type: String,
-        required: [true, "Problem description is required"],
-    },
-    status: {
-        type: String,
-        enum: ["pending", "completed"],
-        default: "pending",
-    },
-    category: {
-        type: String,
-        required: [true, "Category is required"],
-        enum: {
-            values: ["desktop", "laptop", "printer", "plotter", "scanner", "server"],
-            message: "Invalid category",
-        }
-    },
-    itemName: {
-        type: String,
-        required: [true, "Item name is required"],
-    },
-    itemModelNumber: {
-        type: String,
-    },
-    engineerName: {
-        type: String
-    },
-    engineersAssigned: [{
-        type: Schema.Types.ObjectId,
-        ref: "Engineer"
-    }],
-    closedAt: {
-        type: Date
-    },
-    customerRemark: {
-        type: String
-    },
-    engineerRemark: {
-        type: String
-    },
-    itemStatus: {
-        type: String,
-        enum: ["required", "pending"],
-    }
-}, {timestamps: true});
+    { timestamps: true }
+);
 
 export const Call = mongoose.model("Call", callSchema);
