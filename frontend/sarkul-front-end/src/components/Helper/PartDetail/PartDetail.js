21d8431e35293d61ddc733bc95ac70948917ff60
FaizanHashmi3 <96041026+FaizanHashmi3@users.noreply.github.com>
2024-07-05 23:54:23 +0530

    Part transaction updated

--- frontend/sarkul-front-end/src/components/Helper/PartDetail/PartDetail.js
+++ frontend/sarkul-front-end/src/components/Helper/PartDetail/PartDetail.js
@@ -5,22 +5,37 @@ function PartDetail({ item }) {
     return (
         <div>
             {item.category == 'b2e' ? (<Stack border={'2px solid var(--red)'} mt={'15px'} textAlign={'start'} padding={'9px'} direction={'row'} gap={'90px'} borderRadius={'10px'}>
-                
-                    <Stack gap={'10px'}>
-                        <Typography variant='h5' color={'var(--black)'}> {(item.category == 'b2e') ? "Branch To Engineer" : "Engineer To Branch"}</Typography>
-                        <Typography variant='h5'>Call Number: {item.callId} </Typography>
-                        <Typography variant='h5'>Date: {item.createdAt.slice(0, 10)} </Typography>
-                        <Typography variant='h5'>Stock Id: {item.stockId} </Typography>
-                        <Typography variant='h5'>Model Number: {item.modelNumber} </Typography>
-                    </Stack>
-                    <Stack gap={'10px'}>
-                        <Typography variant='h5'>Serial Number: {item.serialNumber} </Typography>
-                        <Typography variant='h5'>Item Name: {item.itemName} </Typography>
-                        <Typography variant='h5'>Part Name: {item.partName} </Typography>
-                        <Typography variant='h5'>Engineer Name: {item.engineerName} </Typography>
-                    </Stack>
-                
-            </Stack>) : (<Stack border={'2px solid var(--green)'} mt={'15px'} textAlign={'start'} padding={'9px'} direction={'row'} gap={'90px'} borderRadius={'10px'}>Work in progress</Stack>)}
+
+                <Stack gap={'10px'}>
+                    <Typography variant='subtitle1' color={'var(--black)'}> {(item.category == 'b2e') ? "Branch To Engineer" : "Engineer To Branch"}</Typography>
+                    <Typography variant='subtitle1'>Call Number: {item?.callId} </Typography>
+                    <Typography variant='subtitle1'>Date: {item?.createdAt.slice(0, 10)} </Typography>
+                    <Typography variant='subtitle1'>Stock Id: {item?.stockId} </Typography>
+                    <Typography variant='subtitle1'>Model Number: {item?.modelNumber} </Typography>
+                </Stack>
+                <Stack gap={'10px'}>
+                    <Typography variant='subtitle1'>Serial Number: {item?.serialNumber} </Typography>
+                    <Typography variant='subtitle1'>Item Name: {item?.itemName} </Typography>
+                    <Typography variant='subtitle1'>Part Name: {item?.partName} </Typography>
+                    <Typography variant='subtitle1'>Engineer Name: {item?.engineerName} </Typography>
+                </Stack>
+
+            </Stack>) : (<Stack border={'2px solid var(--green)'} mt={'15px'} textAlign={'start'} padding={'9px'} direction={'row'} gap={'90px'} borderRadius={'10px'}>
+                <Stack gap={'10px'}>
+                    <Typography variant='subtitle1' color={'var(--black)'}> {(item.category == 'b2e') ? "Branch To Engineer" : "Engineer To Branch"}</Typography>
+                    <Typography variant='subtitle1'>Call Number: {item.callId} </Typography>
+                    <Typography variant='subtitle1'>Date: {item.createdAt.slice(0, 10)} </Typography>
+                    <Typography variant='subtitle1'>Stock Id: {item.stockId} </Typography>
+                    <Typography variant='subtitle1'>Model Number: {item.modelNumber} </Typography>
+                </Stack>
+                <Stack gap={'10px'}>
+                    <Typography variant='subtitle1'>Serial Number: {item.serialNumber} </Typography>
+                    <Typography variant='subtitle1'>Item Name: {item.itemName} </Typography>
+                    <Typography variant='subtitle1'>Part Name: {item.partName} </Typography>
+                    <Typography variant='subtitle1'>Status: {item?.partStatus} </Typography>
+                    <Typography variant='subtitle1'>Engineer Name: {item.engineerName} </Typography>
+                </Stack>
+            </Stack>)}
 
         </div>
     )
--- frontend/sarkul-front-end/src/components/PartTransaction/PartTransactionDetail.js
+++ frontend/sarkul-front-end/src/components/PartTransaction/PartTransactionDetail.js
@@ -3,17 +3,44 @@ import React, { useEffect, useState } from 'react'
 import Loader from '../Loader';
 import SideBarPart from '../Sidebar/SideBarPart';
 import PartDetail from '../Helper/PartDetail/PartDetail';
-import { Stack } from '@mui/material';
+import { Button, MenuItem, Stack, TextField, Typography } from '@mui/material';
 
 function PartTransactionDetail() {
     const [isLoading, setIsLoading] = useState(false)
+    const [eng, setEng] = useState([]);
+    const [state, setState] = useState({
+        selectedEngineer: '',
+        callNumber: '',
+        selectedFilter: ''
+    })
     const [arr, setArr] = useState([]);
+    const [masterArr, setMasterArr] = useState([]);
+
 
     useEffect(() => {
         getData();
     }, [])
 
-
+    useEffect(() => {
+        getEngineerName();
+    }, [])
+    async function getEngineerName() {
+        try {
+            let token = sessionStorage.getItem("accessToken");
+            let url = 'https://sarkultechapi.onrender.com/api/v1/engineer?status=active';
+            const config = {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            };
+            let response = await axios.get(url, config);
+            setEng(response.data.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
     async function getData() {
         setIsLoading(true)
         try {
@@ -29,7 +56,7 @@ function PartTransactionDetail() {
 
             console.log(response.data.data, 'part');
             setArr(response.data.data);
-
+            setMasterArr(response.data.data)
             setIsLoading(false)
         }
         catch (error) {
@@ -40,14 +67,108 @@ function PartTransactionDetail() {
 
     }
 
+    const handleFilterEngineer = (name) => {
+
+        let temp = masterArr.slice(0, masterArr.length)
+        let filteredArr = temp.filter((item) => item.engineerName.toLowerCase() == name.toLowerCase())
+        setArr(filteredArr)
+
+    }
+
+    const handleFilterCallId = (id) => {
+        let temp = masterArr.slice(0, masterArr.length)
+        let filteredArr = temp.filter((item) => item.callId.includes(id))
+        setArr(filteredArr)
+    }
+
     return (
         <div>
             <SideBarPart />
-            {arr.map((item) => {
-                return <Stack alignItems={'center'}> <PartDetail item={item} /> </Stack>
+            <Stack direction={"row"} p={0.7} bgcolor={"white"} justifyContent={'center'} gap={5} >
+                <Stack direction={'row'} gap={3}>
+                    <TextField
+                        select
+                        label='Filter By:'
+                        value={state.selectedFilter}
+                        onChange={(e) => {
+                            setState((prev) => ({
+                                ...prev,
+                                selectedFilter: e.target.value,
+                                selectedEngineer: '',
+                                callNumber: ''
 
-            })}
+                            }))
+                            getData()
+                        }}
+                        sx={{
+                            width: '130px',
+                        }}
+                    >
+                        <MenuItem value={'engineer'}>Engineer</MenuItem>
+                        <MenuItem value={'callNumber'}>Call Number</MenuItem>
+                    </TextField>
+                    {state.selectedFilter == 'engineer' &&
+                        <TextField
+                            select
+                            value={state.selectedEngineer}
+                            label='Engineers'
+                            onChange={(e) => {
+                                setState((prev) => ({
+                                    ...prev,
+                                    selectedEngineer: e.target.value
+                                }))
+                                handleFilterEngineer(e.target.value)
+                            }}
+                            sx={{
+                                width: '230px',
+                            }}
+                        >
+                            {
 
+                                eng.map((item) => {
+                                    return <MenuItem value={item.employeeName}>{item.employeeName}</MenuItem>
+                                })
+
+                            }
+                        </TextField>}
+
+                    {state.selectedFilter == 'callNumber' && <TextField
+                        label='Call Number'
+                        value={state.callNumber}
+                        onChange={(e) => {
+                            setState((prev) => ({
+                                ...prev,
+                                callNumber: e.target.value
+                            }))
+                            handleFilterCallId(e.target.value)
+                        }}
+                        sx={{
+                            width: '150px',
+                        }}
+                    />}
+                    <Button
+                        onClick={() => {
+                            setState({
+                                selectedEngineer: '',
+                                callNumber: '',
+                                selectedFilter: ''
+                            })
+                            getData()
+                        }}
+                    >Clear Filter</Button>
+                </Stack>
+
+
+
+            </Stack>
+            {isLoading ? <Loader /> :
+
+                arr.map((item) => {
+                    return <Stack alignItems={'center'}> <PartDetail item={item} /> </Stack>
+
+                })
+
+            }
         </div>
     )
 }
